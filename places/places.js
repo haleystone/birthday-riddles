6ccd8794e6fa315418a2aee4d73442d18042533c
Haley Stone <130417556+haleystone@users.noreply.github.com>
2026-07-06 22:59:54 +0100

    Add rounds data for Sydney and Glasgow

---
+++ places/places.js
@@ -0,0 +1,18 @@
+const rounds = [
+  {
+    image: "images/sydney.jpg",
+    placeName: "Sydney",
+    latitude: -33.8688,
+    longitude: 151.2093,
+    month: "March",
+    year: "2023"
+  },
+  {
+    image: "images/glasgow.jpg",
+    placeName: "Glasgow",
+    latitude: 55.8642,
+    longitude: -4.2518,
+    month: "October",
+    year: "2024"
+  }
+];
